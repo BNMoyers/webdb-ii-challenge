838b4acb717a2314b99b379ddbec58ae71e72107
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-11-05 14:31:57 -0600

    created table using knex

--- data/migrations/20191105142136_add_cars_table.js
+++ data/migrations/20191105142136_add_cars_table.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', table => {
+      table.increments();
+      table.string('vin', 20).notNullable();
+      table.string('make', 64).notNullable();
+      table.string('model', 255).notNullable();
+      table.integer('mileage').notNullable();
+      table.string('transmission', 256);
+      table.string('title status', 256);
+      
+
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
